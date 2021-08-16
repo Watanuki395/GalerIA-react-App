dda2a60e8daa2ddb324fbfecdc242c95857a7f34
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2021-08-16 11:08:53 -0600

    adding build

---
+++ client/build/precache-manifest.5153c10f9b913503a65f3ef260b27d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c493063e693416685c74453267f5dc82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7621a8f04435bcea2596",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5192eab0f21fa02ac609",
+    "url": "/static/js/2.814ad09c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.814ad09c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7621a8f04435bcea2596",
+    "url": "/static/js/main.950a66cd.chunk.js"
+  },
+  {
+    "revision": "ae863b872dc9b20a0836",
+    "url": "/static/js/runtime-main.5e757d4b.js"
+  }
+]);
