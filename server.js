c256b6327d76b1da56dc6a40cd9098df4def29f2
Gerardo Salas Montoya <gesalas@cr.ibm.com>
2021-08-16 21:26:07 -0600

    adding client

--- server.js
+++ server.js
@@ -8,7 +8,29 @@ const path = require('path')
 
 const app = express(); 
 var router = express.Router();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
+
+
+// --> Add this
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://galleria-spa-dev.herokuapp.com/']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(helmet())
+// --> Add this
+app.use(cors(corsOptions))
+
+
 
 app.engine(
     ".hbs",
@@ -35,10 +57,12 @@ app.use(cors());
 app.use('/',router);
 //app.get('/', (req, res) => { res.status(200).send('Server is working.') }); 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("app/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "app", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
